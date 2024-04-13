c7e7e689b20c05b8aa8964261b91b19dfd49b25e
Christian Almli <chrisalm97@gmail.com>
2024-04-13 22:47:03 +0200

    Complete e2e tests, check workflow run

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,21 +10,19 @@ describe('Login functionality', () => {
         cy.get('#loginPassword').type('validpassword');
         cy.get('#loginForm').submit();
 
-
+        // Assert that logout button is visible indicating successful login
         cy.get('button[data-auth="logout"]').should('be.visible');
     });
-});
 
-describe('Login functionality with valid credentials', () => {
     it('prevents login and shows a message with invalid credentials', () => {
         cy.visit('/');
-        
+    
         // Open login modal
         cy.get('button[data-auth="login"]').click();
 
         // Fill the login form and submit
-        cy.get('#loginEmail').type('validuser@example.com');
-        cy.get('#loginPassword').type('validpassword');
+        cy.get('#loginEmail').type('invaliduser@example.com');
+        cy.get('#loginPassword').type('invalidpassword');
         cy.get('#loginForm').submit();
 
         // Check for an error message
