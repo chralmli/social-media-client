c6f5464d55f2b9aed603381765dcc2543484adca
Christian Almli <59100233+chralmli@users.noreply.github.com>
2024-04-14 19:07:52 +0200

    Revert "Initial Project Setup with ESLint, Prettier, and CI/CD Configurations"

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',  // Setting environment to jsdom
-    transform: {
-      '^.+\\.js$': 'babel-jest'  // Transform JavaScript files using babel-jest
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/']  // Ignore node_modules for transformation
-  };
-  
