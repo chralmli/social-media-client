9e8001a859dfeb6d44968351545b1cdbbfe40ebd
Christian Almli <chrisalm97@gmail.com>
2024-04-14 23:17:09 +0200

    fix apiBase test

--- src/js/api/apiBase.js
+++ src/js/api/apiBase.js
@@ -1,2 +1,1 @@
 export const apiBase = new URL('https://whatver.com/api/posts');
-export const url = new URL(apiBase);
--- src/test/jest/__tests__/apiBase.test.js
+++ src/test/jest/__tests__/apiBase.test.js
@@ -1,4 +1,4 @@
-import { apiBase, url } from '../../../js/api/apiBase.js';
+import { apiBase } from '../../../js/api/apiBase.js';
 
 describe('apiBase.js', () => {
     it('should define apiBase as a URL object', () => {
@@ -7,9 +7,5 @@ describe('apiBase.js', () => {
 
     it('should construct the correct base URL', () => {
         expect(apiBase.toString()).toBe('https://whatver.com/api/posts');
-    });
-
-    it('should create a URL object from the apiBase string', () => {
-        expect(url).toEqual(new URL(apiBase));
     });
 });
